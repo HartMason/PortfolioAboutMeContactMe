3fb78b28e6aa4b198079f0069018c47a320c39e9
HartMason <hartmason85@gmail.com>
2021-10-26 21:17:17 -0500

    needs styling

--- contact.js
+++ contact.js
@@ -1,1 +1,12 @@
-document.getElementById("email").submit();
+const dataSubmission = () => {
+    console.log("you clicked me")
+    alert("You Submitted the Data")
+} 
+
+const Capitalized = () => {
+    console.log("capitalized")
+    let x = document.getElementById("email").value 
+    console.log("when key is pressed",x)
+    x.toUpperCase()  
+}
+
